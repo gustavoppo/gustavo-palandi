84ecd70307df82604fd1b97096af99619eafa80f
Gustavo Palandi <gustavoppo@hotmail.com>
2024-08-06 15:25:57 -0300

    chore: Add Tailwind CSS configuration and update dependencies

---
+++ src/app/components/skills/skills.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gp-skills',
+  standalone: true,
+  imports: [],
+  templateUrl: './skills.component.html',
+  styleUrl: './skills.component.css'
+})
+export class SkillsComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { ToolbarComponent } from '../../components/toolbar/toolbar.component';
+import { AboutComponent } from '../../components/about/about.component';
+import { ContactComponent } from '../../components/contact/contact.component';
+import { SkillsComponent } from '../../components/skills/skills.component';
+import { ContentComponent } from '../../components/content/content.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    ToolbarComponent,
+    AboutComponent,
+    ContactComponent,
+    SkillsComponent,
+    ContentComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
