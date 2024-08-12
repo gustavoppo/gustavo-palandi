f9ffe1905e146a242005f3f49aa67ccd4a0339db
Gustavo Palandi <gustavoppo@hotmail.com>
2024-08-12 08:54:45 -0300

    chore: Update loading component CSS and HTML, and skills component HTML

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'gp-skills',
@@ -8,10 +8,44 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
-
+  selectedSkill: string | null = null;
+  activeButton: HTMLElement | null = null;
   toggleText(event: Event) {
     const element = event.currentTarget as HTMLElement;
-    element.classList.toggle('hover-active');
-    console.log(element);
+    const skill = element.getAttribute('data-level');
+
+    // Se o mesmo botão for clicado, desmarque-o
+    if (this.activeButton === element) {
+      this.resetSelection();
+      return;
+    }
+
+    // Desmarque o botão anterior se existir
+    if (this.activeButton) {
+      this.activeButton.classList.remove('hover-active');
+    }
+
+    // Atualize o botão ativo e o skill selecionado
+    element.classList.add('hover-active');
+    this.selectedSkill = skill;
+    this.activeButton = element;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+
+    // Verifica se o clique foi fora do botão ativo
+    if (this.activeButton && !this.activeButton.contains(target)) {
+      this.resetSelection();
+    }
+  }
+
+  private resetSelection() {
+    if (this.activeButton) {
+      this.activeButton.classList.remove('hover-active');
+    }
+    this.selectedSkill = null;
+    this.activeButton = null;
   }
 }
